2cf5878fff85fa3a41d50204f5667a882cfb13d1
Juan Pablo Vaccarezza <juanpirvaccarezza@gmail.com>
2026-02-28 16:45:53 -0300

    Initial structure of the system Smart Sheet with Spring Boot.

---
+++ src/test/java/com/smart_sheet/smart_sheet/SmartSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_sheet.smart_sheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
